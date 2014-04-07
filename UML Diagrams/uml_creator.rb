edeeb354fd54b6cb02dc1ce5d2588a84faf80899
Alistair McKinnell <alistair.mckinnell@gmail.com>
2014-04-06 23:09:47 -0400

    Complete the script by invoking UmlGraph on the ERB result.

--- UML Diagrams/uml_creator.rb
+++ UML Diagrams/uml_creator.rb
@@ -3,16 +3,24 @@ require 'erb'
 class UmlCreator
 
   def initialize(filename)
-    @f = File.open(filename)
+    @filename = filename
   end
 
   def create
-    template_file = File.open(@f, 'r').read
+    template_file = File.open(@filename, 'r').read
     erb = ERB.new(template_file)
-    File.open('IcgRoot.java', 'w+') { |file| file.write(erb.result) }
+    File.open(output_filename, 'w+') { |file| file.write(erb.result) }
+  end
+
+  private
+
+  def output_filename
+    @filename.gsub(/.erb/, '')
   end
 
 end
 
 creator = UmlCreator.new('IcgRoot.erb.java')
 creator.create
+
+system('./umlgraph IcgRoot png')
