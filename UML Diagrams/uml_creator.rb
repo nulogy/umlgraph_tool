4d9a54cd9bcf2f31631cc6f2320ad1b85bf62298
Alistair McKinnell <alistair.mckinnell@gmail.com>
2014-04-09 22:07:28 -0400

    Extract the options to a YAML file.

--- UML Diagrams/uml_creator.rb
+++ UML Diagrams/uml_creator.rb
@@ -1,29 +1,10 @@
 require 'erb'
+require 'yaml'
 
 class Keywords
 
   def initialize
-    @options = {
-      :color => {
-        :base => 'lightgray',
-        :domain => 'lightyellow',
-        :drivers => 'lightcoral',
-        :interpreters => 'gold',
-        :rules => 'lightblue',
-        :scopes => 'violet',
-        :sync => 'khaki',
-        :transitions => 'cyan',
-        :validators => 'palegreen'
-      },
-
-      :hidden => {
-        :concrete_drivers => true,
-        :concrete_scopes => true,
-        :concrete_validators => true,
-        :sync_details => true,
-        :wizard => true
-      }
-    }
+    @options = YAML.load_file('config.yml')
   end
 
   def bind
@@ -31,13 +12,22 @@ class Keywords
   end
 
   def node_fill_color(color_key)
-    "@opt nodefillcolor #{@options[:color][color_key]}"
+    "@opt nodefillcolor #{color_option(color_key)}"
   end
 
   def show(visibility_key)
-    "@hidden" if @options[:hidden][visibility_key]
+    "@hidden" unless visibility_option(visibility_key)
   end
 
+  private
+
+  def color_option(color_key)
+    @options[:color.to_s][color_key.to_s]
+  end
+
+  def visibility_option(visibility_key)
+    @options[:visible.to_s][visibility_key.to_s]
+  end
 end
 
 class UmlCreator
