61c51ed7344f26c7edb7d8210f99034c9becd0ae
Jason Cheong-Kee-You <jasonc@nulogy.com>
2014-05-03 00:14:21 -0400

    Simplify configuration. Move all yml configuration into main template file.

--- src/uml_creator.rb
+++ src/uml_creator.rb
@@ -1,44 +1,14 @@
 require 'erb'
 require 'yaml'
 
-class Keywords
-  def initialize(args)
-    @colors = YAML.load_file(args[:colors])
-    @diagram = YAML.load_file(args[:diagram])
-    @groups = YAML.load_file(args[:template_filename])
-    @show = YAML.load_file(args[:show])
-  end
-
-  def bind
-    binding
-  end
-
-  def diagram_options
-    @diagram['diagram'].map { |k| "@opt #{k}" }.join("\n * ")
-  end
-
-  def node_fill_color(color_key)
-    "@opt nodefillcolor #{color_option(color_key)}"
-  end
-
-  def show?(classname)
-    @show[classname.split.first]
-  end
-
-  private
-
-  def color_option(color_key)
-    @colors.fetch(color_key.to_s, 'white')
-  end
-end
-
 class UmlCreator
   def initialize(args)
     @args = args
+    @groups = YAML.load_file(args[:template_filename])
   end
 
   def create
-    template = read_file('templates/template.erb')
+    template = read_file('src/template.erb')
     rendered = render_template(template)
     write_file(@args[:output_filename], rendered)
   end
@@ -50,7 +20,7 @@ class UmlCreator
   end
 
   def render_template(template)
-    ERB.new(template).result(Keywords.new(@args).bind)
+    ERB.new(template).result(binding)
   end
 
   def write_file(filename, contents)
